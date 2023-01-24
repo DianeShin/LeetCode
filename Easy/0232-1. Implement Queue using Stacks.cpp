9fa7be63ba000bc1ee4492b91c2c82b3a2a702a9
DianeShin <jadore845@snu.ac.kr>
2023-01-24 19:58:08 +0100

    0232 Update

--- Easy/0232-1. Implement Queue using Stacks.cpp
+++ Easy/0232-1. Implement Queue using Stacks.cpp
@@ -1,3 +1,43 @@
+/* 
+Question link : https://leetcode.com/problems/implement-queue-using-stacks/
+
+Runtime : 4 ms
+Beats : 30.96%
+Memory : 7MB
+Beats : 81.79%
+
+Time Complexity :
+push : O(1) : only need to push in main_stack.
+pop : O(n) : need to remove and save n-1 elements in sub_stack, remove element, then move it back to main_queue.
+top : O(n) : need to remove and save n-1 elements in sub_stack, save element, then move it back to main_queue.
+empty : O(1) : only need to check positions of pointers.
+
+Space Complexity : 
+push : O(n) : 1 main_stack and 1 int.
+pop : O(n) : 2 stacks and 3 for i, elem_num, result.
+top : O(n) : 2 stacks and 3 for i, elem_num, result.
+empty : O(n) : other than existing queue to remove, no additional space needed.
+
+Algorithm :
+push :
+1. push to main_stack.
+pop :
+1. Save the main_stack elements to sub_stack until 1 element left(the element desired to pop). Saved elements are popped.
+2. Save the left element in a separate int container, then pop it from main_stack.
+3. Move the sub_stack elements to main_stack.
+4. return result.
+top :
+1. Save the main_stack elements to sub_stack until 1 element left(the element desired to pop). Saved elements are popped.
+2. Save the left element in a separate int container.
+3. Move the sub_stack elements to main_stack.
+4. return result.
+empty :
+1. Empty the main_stack.
+
+Point :
+1. When moving elem, be careful to define loop condition in prior. Stack size changes by pop, so using stack.size() in loop condition will malfunction.
+*/
+
 #include <stack>
 
 using namespace std;
@@ -17,8 +57,8 @@ public:
     
     int pop() {
         // move n-1 elems to sub_stack
-        int curr_stack_size = main_stack.size();
-        for (int i = 0; i < curr_stack_size -1; i++){
+        int elem_num = main_stack.size();
+        for (int i = 0; i < elem_num -1; i++){
             sub_stack.push(main_stack.top());
             main_stack.pop();
         }
@@ -30,8 +70,8 @@ public:
         main_stack.pop();
 
         // move n-1 elem back to main_stack
-        curr_stack_size = sub_stack.size();
-        for (int i = 0; i < curr_stack_size; i++){
+        elem_num = sub_stack.size();
+        for (int i = 0; i < elem_num; i++){
             main_stack.push(sub_stack.top());
             sub_stack.pop();
         }       
@@ -42,8 +82,8 @@ public:
     
     int peek() {
         // move n-1 elems to sub_stack
-        int curr_stack_size = main_stack.size();
-        for (int i = 0; i < curr_stack_size -1; i++){
+        int elem_num = main_stack.size();
+        for (int i = 0; i < elem_num -1; i++){
             sub_stack.push(main_stack.top());
             main_stack.pop();
         }
@@ -52,8 +92,8 @@ public:
         int elem = main_stack.top();
 
         // move n-1 elem back to main_stack
-        curr_stack_size = sub_stack.size();
-        for (int i = 0; i < curr_stack_size; i++){
+        elem_num = sub_stack.size();
+        for (int i = 0; i < elem_num; i++){
             main_stack.push(sub_stack.top());
             sub_stack.pop();
         }       
